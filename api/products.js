d3d9a6c39202b7876b30133cae90577703d32b18
Chandan <srivstavachandan01@gmail.com>
2024-09-11 23:09:20 +0530

    Initial commit

---
+++ api/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    "id": 1,
+    "name": "Half T Shirts",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 350,
+    "stock": 50,
+    "description": "Symbol Men's Cotton T Shirt | Round Neck | Half Sleeve | Plain - Regular Fit (Available in Plus Size).",
+    "image": "../public/t shite photo.png"
+  },
+  {
+    "id": 2,
+    "name": "Track Pants",
+    "category": "Garments",
+    "brand": "Shree ji",
+    "price": 499.99,
+    "stock": 50,
+    "image": "../public/track lower.png",
+    "description": "Black Boldfit Track Pant for Men Slim Fit Joggers for Men for Running, Gym Sports Lower for Men & Boys Summer Track Pants."
+  },
+  {
+    "id": 3,
+    "name": "Full T-shirts",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 149.99,
+    "stock": 30,
+    "image": "../public/full t shirts.png",
+    "description": "High-quality wireless headphones with over-ear design, 20 hours of battery life, and a sleek black color."
+  },
+  {
+    "id": 4,
+    "name": "Hoodie",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 450,
+    "stock": 20,
+    "image": "../public/sky hoodie.png",
+    "description": " SKY colour Alan Jones Clothing Men's Cotton Oversize Hooded Hoodie."
+  },
+  {
+    "id": 5,
+    "name": "Full Shirts",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 350,
+    "stock": 30,
+    "image": "../public/white full shirts.png",
+    "description": "IndoPrimo Men's Regular Fit Fancy Double Pocket Casual Shirt for Men Full Sleeves - Cargo."
+  },
+  {
+    "id": 6,
+    "name": "jacket",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 500,
+    "stock": 20,
+    "image": "../public/full jacket.png",
+    "description": "Smartwatch with a 1.3-inch AMOLED display, water-resistant design, fitness tracking features, and a stylish silver color."
+  },
+  {
+    "id": 6,
+    "name": "jacket",
+    "category": "Garment",
+    "brand": "Shree ji",
+    "price": 500,
+    "stock": 20,
+    "image": "../public/full jacket.png",
+    "description": "Smartwatch with a 1.3-inch AMOLED display, water-resistant design, fitness tracking features, and a stylish silver color."
+  }
+
+]
+
+export default products
---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+// import products from "./api/products.js";
+import products from "./api/products";
+import { showProductContainer } from "./homeProductCards";
+
+// Define a function named `showProductContainer` that takes an array of products as input.
+showProductContainer(products);
